d98813bc09cd6b811b1d713cb86ae7972a4cfe96
FreedCompanyFree <106900528+FreedCompanyFree@users.noreply.github.com>
2022-08-31 01:58:08 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,15 +38,15 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"version.json": "c76e018fd40a7a070eb937ec73bd886f",
+"version.json": "6e97a2a36a30a1319f7300fda5ad6f93",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e202dd759d2055fe99bfb810c910456c",
-"/": "e202dd759d2055fe99bfb810c910456c",
-"main.dart.js": "71a0423c97fe4999bff98a58609aa35b",
+"index.html": "d304830b45db193cf8984412726e9c15",
+"/": "d304830b45db193cf8984412726e9c15",
+"main.dart.js": "967f1e81f8629f4047a35a9a136b7dce",
 "manifest.json": "eda834508ff2ad1195c4adafd3b9dfed"
 };
 
